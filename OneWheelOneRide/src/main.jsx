a65c3029c0867bf2264ab100b99514daa1ab968d
Steven Underwood <stevenunderwood2@hotmail.com>
2023-07-31 14:16:47 -0500

    Add styles.scss to main.jsx

--- OneWheelOneRide/src/main.jsx
+++ OneWheelOneRide/src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from'react-router-dom'
+import '../src/scss/styles.scss'
+import * as bootstrap from 'bootstrap'
 
 
 
--- OneWheelOneRide/vite.config.js
+++ OneWheelOneRide/vite.config.js
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: "/OneWheelOneRide-Front",
   plugins: [react()],
+  resolve: {
+    alias: {
+      '~/boostrap': path.resolve(__dirname, './src/bootstrap'),
+    }
+  }
 })
